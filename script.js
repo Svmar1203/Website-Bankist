604900abb975c095ac7d6ea5ec1f0c24085fbc88
Svetlana <suskevits@gmail.com>
2021-12-19 22:30:25 +0200

    19.12.21

--- script.js
+++ script.js
@@ -141,3 +141,24 @@ h1.onmouseenter = function (e) {
   alert('This is EventLisstener');
 };
 */
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  console.log('Link', e.target);
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log('CONTAINER', e.target);
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  console.log('NAV');
+  this.style.backgroundColor = randomColor();
+});
